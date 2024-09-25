ef28e7d6eca154731fe33e7de130cbf86f0f5966
Luke Pupilli <plph2011@gmail.com>
2024-09-25 18:13:48 -0400

    empty commit

--- src/Scanner.java
+++ src/Scanner.java
@@ -4,3 +4,12 @@ public class Scanner {
 		System.out.println("Hello World");
 	}
 }
+
+
+//Program a scanner in Java based on your finite state machine in step B. Your scanner should:
+//Define a state-transition table in the form of a two-dimensional array.
+//Input a string or character sequence from a user, file, or standard channel.
+
+// Output a stream of tokens to a user, file, or standard channel. Your stream of tokens should:
+	// Represent each token’s class, like the abstract concept of a keyword, operator, identifier, or literal.
+	// Represent each token’s value, if any, like the concrete name of an identifier or numeric value of a literal.
