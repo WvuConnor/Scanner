f6140007f61d382d2108aa3e64e008391766e78e
Steven Poripski <143005825+stevenporip@users.noreply.github.com>
2024-09-26 13:11:12 -0400

    commit 2

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Scanner {
 	enum TokenType{
-		KEYWORD, OPERATOR, IDENTIFIER, LITERAL, PARENTHESIS, BRACKET, SEMICOLON, EOI;
+		KEYWORD, OPERATOR, IDENTIFIER, LITERAL, PARENTHESIS, BRACKET, SEMICOLON, EOI, WHITESPACE, ERROR;
 	}
 	static class Token {
 		TokenType type;
@@ -32,28 +36,89 @@ public class Scanner {
 	
 	//keywords
 	private static final String keywords[] = {"for", "while", "if"};
-	private static final String operators[] = {"+", "++", "==", "--", "&&", "&", "-", "%", "/", "=", "<"};
+	private static final String operators[] = {"+", "++", "==", "--", "&&", "&", "-", "%", "/", "=", "<", ">"};
 	
 	//State Transition Array
 	private static final int STATE_TRANSITION[][]= {
 			{LITERAL, IDENTIFIER, OPERATOR, KEYWORD, LITERAL, PARENTHESIS, BRACKET, WHITESPACE, END_OF_INPUT, ERROR}, //START STATE
-			{ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR}, //State 1: Number State
-			{}, //State 2: IDENTIFIER
-			{}, //State 3: OPERATOR
-			{}, //State 4: KEYWORD
-			{}, //State 5: LITERAL
-			{}, //State 6: PARENTHESIS
-			
-			
+			{LITERAL, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR, }, //State 1: Literal (Number)
+			{ERROR, IDENTIFIER, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR}, //State 2: IDENTIFIER
+			{ERROR, ERROR, OPERATOR, ERROR, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR}, //State 3: OPERATOR
+			{ERROR, ERROR, ERROR, KEYWORD, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR}, //State 4: KEYWORD
+			{ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR}, //State 5: PARENTHESIS
+			{ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR}, //State 6: Bracket
+			{ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, END_OF_INPUT, ERROR}, //State 7: Whitespace
+			{ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR}, //State 8: End_OF_INPUT
+			{ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR}, //ERROR STATE	
 	};
-	
-	
-	
-	
 	public static void main(String args[]) {
-		
-		
-		
+		String input = "123";
+		List<Token> tokens = scan(input);
+		for(Token token : tokens){
+			System.out.println(token);
+		}
 	}//end main
+
+	//Method to scan given input
+	//Param input string
+	//Return List of tokens
+	private static List<Token> scan(String input){
+		List<Token> tokens = new ArrayList<>();
+		StringBuilder currentToken = new StringBuilder();
+		int currentState = START;
+
+		for(char c : input.toCharArray()){
+				switch(currentState){
+					case START:
+							if(Character.isDigit(c)){
+								currentState = LITERAL;
+								currentToken.append(c);
+							}else if(Character.isLetter(c)){
+								currentState = IDENTIFIER;
+								currentToken.append(c);
+							}else if(isOperator(c)){
+								currentState = OPERATOR;
+								currentToken.append(c);
+								tokens.add(new Token(TokenType.OPERATOR, currentToken.toString()));
+								currentToken.setLength(0);
+								currentState = START;
+							}else if(c == '(' || c == ')'){
+								currentState = PARENTHESIS;
+								currentToken.append(c);
+								tokens.add(new Token(TokenType.PARENTHESIS, currentToken.toString()));
+								currentToken.setLength(0);
+								currentState = START;
+							}else if(c == '{' || c == '}'){
+								currentState = BRACKET;
+								currentToken.append(c);
+								tokens.add(new Token(TokennType.BRACKET, currentToken.toString()));
+							}
+				}
+		}
+	
+
+
+
+
+		return tokens;
+	}
+
+	private static Boolean isKeyword(String input){
+		for(String keyword : keywords){
+			if(keyword.equals(input)){
+				return true;
+			}
+		}
+		return false;
+	}
+	private static Boolean isOperator(char c){
+		for(String op : operators){
+			if(op.indexOf(c) != -1){
+				return true;
+			}
+		}
+		return false;
+	}
+
 }//end scanner class
 
